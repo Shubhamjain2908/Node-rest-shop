e0ea30b73dcfa8ec02208cbb69dc077c79de3811
shubham jain <shubhamjain2908@gmail.com>
2018-06-07 21:54:07 +0530

    Handling Errors & Improving the Project Setup

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
+const morgon = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
+
+//This tells express to log via morgan
+app.use(morgon('dev'));
+
 /**
  * middleware for a node app    
+ * Routes which should handle requests
  */
 app.use('/products', productRoutes);    // get to product route
 app.use('/orders', orderRoutes);
 
+//error handling : other than above requests
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);    // forward error reqest
+});
+
+// errors thrown from anywhere in this application
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 module.exports = app;
