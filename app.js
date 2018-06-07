aa08e6b503d90327e966bac271768176eb5c8e6e
shubham jain <shubhamjain2908@gmail.com>
2018-06-07 22:23:21 +0530

    Handling CORS

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.use(morgon('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 /**
  * middleware for a node app    
  * Routes which should handle requests
