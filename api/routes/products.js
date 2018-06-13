0fb2bd2faf64e143b76386041232c3c1040d01a8
shubham jain <shubhamjain2908@gmail.com>
2018-06-14 00:22:23 +0530

    Mongoose Validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,15 +5,29 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    Product.find().exec().then(docs => {
+    Product.find().select('name price _id').exec().then(docs => {
         console.log(docs);
-        if (docs.length > 0) {
-            res.status(200).json(docs);    
-        } else {
-            res.status(404).json({
-                message: 'No entries found'
-            });
-        }
+        const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + doc._id
+                    }
+                }
+            })
+        };
+        //if (docs.length > 0) {
+            res.status(200).json(response);    
+        // } else {
+        //     res.status(404).json({
+        //         message: 'No entries found'
+        //     });
+        // }
         
     }).catch(err => {
         console.log(err);
@@ -30,9 +44,17 @@ router.post('/', (req, res, next) => {
     //.exec(); // exec will convert it in Promise
     product.save().then((result) => {
         console.log(result);
-        res.status(200).json({
-            message: 'Handling POST requests to /products',
-            createdProduct: result
+        res.status(201).json({
+            message: 'Product created successfully',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
         });
     }).catch(err => {
         console.log(err);
@@ -43,11 +65,17 @@ router.post('/', (req, res, next) => {
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.findById(id).exec()
+    Product.findById(id).select('name price _id').exec()
         .then(doc => {
             console.log('From database', doc);
             if(doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + doc._id
+                    }
+                });
             } else {
                 res.status(404).json({message: 'No valid entry found for current Id'});
             }
@@ -68,7 +96,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({_id: id}, {$set: updateOps}).exec() // $set - by default mongoose object
         .then(result => {
             console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
@@ -78,7 +112,14 @@ router.patch('/:productId', (req, res, next) => {
 
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.findOneAndRemove({ _id: id }).exec().then(result => res.status(200).json(result))
+    Product.findOneAndRemove({ _id: id }).exec().then(result => res.status(200).json({
+        message: 'Product Deleted',
+        request: {
+            type: 'POST',
+            url: 'http://localhost:3000/products',
+            body: { name: 'String', price: 'Number' } 
+        }
+    }))
         .catch(err => {
             console.log(err);
             res.status(500).json({error: err});
